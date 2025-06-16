23c95c5c36fc7f9936afcf35a0bdda9caa47fd54
Satendra Singh <ersatendrasingh1325@gmail.com>
2025-06-16 18:59:39 +0530

    feat(wip): set up individual DB instances per service

--- user-service/src/config/db/db.ts
+++ user-service/src/config/db/db.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import env from "../../utils/env.js";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: env.DATABASE_URL,
+});
 
 export default prisma;
